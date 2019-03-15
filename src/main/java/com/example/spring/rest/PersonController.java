1b368eace9dfd501d2f90901b42b472edb620ed7
Willans Firmo <willansfirmo@gmail.com>
2019-03-15 01:41:28 -0300

    CrossOrigins.

--- src/main/java/com/example/spring/rest/PersonController.java
+++ src/main/java/com/example/spring/rest/PersonController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import com.example.spring.service.PersonService;
  * @since 13/03/2019
  *
  */
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(value = "/api/person")
 public class PersonController {
